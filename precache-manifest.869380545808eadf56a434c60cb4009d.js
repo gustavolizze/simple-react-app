e2797f501335c4d74cf0b4c9173f6b6e0c00e835
Gustavo Lizze <gustavolizze@hotmail.com>
2019-02-05 23:14:51 -0200

    Updates

---
+++ precache-manifest.869380545808eadf56a434c60cb4009d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "daa46f32ffa9b5d94ce6",
+    "url": "/simple-react-app/static/js/runtime~main.daa46f32.js"
+  },
+  {
+    "revision": "6e0ffa0465aa92b46709",
+    "url": "/simple-react-app/static/js/main.6e0ffa04.chunk.js"
+  },
+  {
+    "revision": "ffc7e4e801d837b38448",
+    "url": "/simple-react-app/static/js/1.ffc7e4e8.chunk.js"
+  },
+  {
+    "revision": "a63521e504dd3d356be0bf8a8df46f1f",
+    "url": "/simple-react-app/index.html"
+  }
+];
